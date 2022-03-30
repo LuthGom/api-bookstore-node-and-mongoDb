d0829b3a42d88b827d4e8fc2571db5b7e284201b
LuthGom <lucianomendes23@yahoo.com>
2022-03-30 00:48:25 -0300

    Implmentando conexão com o banco MongoDb utilizando o mongoose e criação da primeira modelagem da model

---
+++ src/models/Livro.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema(
+    {
+        id: { type: String },
+        titulo: { type: String, required: true },
+        autor: { type: String, required: true },
+        editora: { type: String, required: true },
+        numeroPaginas: { type: Number }
+
+    }
+);
+
+const books = mongoose.model('books', bookSchema);
+
+export default books;
