4ba7d685590f79fddb7461018eac594dae240a72
LuthGom <lucianomendes23@yahoo.com>
2022-04-15 17:49:28 -0300

    limpanndo server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv/config';
 import app from "./src/app.js"
-const port = process.env.PORT ||  3000;
+const port =   3000;
 
 
 app.listen(port, () => console.log(`rodando na porta ${port}`))
